4f0cbd02c4c881391930f9ae2de1eeafe87d79ff
michellenaour <54044891+michellenaour@users.noreply.github.com>
2023-05-10 15:24:11 -0400

    gitpage

--- deply.sh
+++ deply.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'New Deploment'
 
-git push -f git@github.com:michellenaour/vitydashboard.git master:main
+git push -f git@github.com:michellenaour/citydashboard.git master:main
 #d
 cd -
